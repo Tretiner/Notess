362e9a1c1cc5651c6866dedc554909479b2017af
Tretiner <alexandirina@inbox.ru>
2024-04-28 12:56:54 +0500

    Where it all begins..

---
+++ Notess/Data/ApplicationUser.cs
@@ -0,0 +1,6 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Notess.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public sealed class ApplicationUser : IdentityUser;
