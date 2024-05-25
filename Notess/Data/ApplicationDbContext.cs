b294039683fcdc37bc06d63f02ba910a9ab250db
Tretiner <alexandirina@inbox.ru>
2024-05-25 15:00:28 +0500

    Deleted unneded code

--- Notess/Data/ApplicationDbContext.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Notess.Data;
-
-public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options);
--- Notess/Data/ApplicationUser.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Notess.Data;
-
-// Add profile data for application users by adding properties to the ApplicationUser class
-public sealed class ApplicationUser : IdentityUser;
